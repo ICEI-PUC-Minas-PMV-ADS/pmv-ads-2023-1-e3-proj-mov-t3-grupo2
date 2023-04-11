e36db8582c7325b8d8aaaf6f63fed8ad939cbf25
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-11 19:48:06 -0300

    Adiciona Status Bar

--- finance_news/App.tsx
+++ finance_news/App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
@@ -11,6 +12,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <News /> : <Loading />}
     </ThemeProvider>
   )
