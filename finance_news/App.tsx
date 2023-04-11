88db0351fa41b9d0f7150d71d095f28140d35ce9
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-11 19:46:32 -0300

    Adiciona Google Fonts e Loading Component

--- finance_news/App.tsx
+++ finance_news/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import theme from './src/themes'
 
 import { News } from '@screens/News'
+import { Loading } from '@components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <News />
+      {fontsLoaded ? <News /> : <Loading />}
     </ThemeProvider>
   )
 }
