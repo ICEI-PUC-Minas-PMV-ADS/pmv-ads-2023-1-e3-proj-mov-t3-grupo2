e1c45c2bb6cf3cfca78a31f374b35e20f89435dc
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-11 21:58:13 -0300

    Adiciona Highlight component

--- finance_news/src/screens/News/index.tsx
+++ finance_news/src/screens/News/index.tsx
@@ -1,11 +1,18 @@
 
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+
 import { Container } from "./styles";
 
 export function News() {
   return (
     <Container>
       <Header />
+
+      <Highlight
+        title="Últimas Notícias"
+        subtitle="clique no link para ser redirecionado"
+      />
     </Container>
   )
 }
