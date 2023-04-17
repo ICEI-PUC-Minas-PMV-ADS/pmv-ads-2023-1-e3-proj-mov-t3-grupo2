7c81154e91084ec11853f773c86fdd8cfc124741
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-16 22:58:18 -0300

    Adiciona Navegacao em Header Component

--- finance_news/src/components/Header/index.tsx
+++ finance_news/src/components/Header/index.tsx
@@ -1,19 +1,38 @@
-import { Container, BackIcon, BackButton } from './styles';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
 import { Logo } from '@components/Logo';
+
+import { Container, BackIcon, BackButton } from './styles';
 
 type HeaderProps = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  function handleGoToNewsList() {
+    if (route.name === 'news') return
+
+    navigation.navigate('news');
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
-      <Logo size={showBackButton ? 56 : 84} />
+      <TouchableOpacity onPress={handleGoToNewsList}>
+        <Logo size={showBackButton ? 56 : 84} />
+      </TouchableOpacity>
     </Container>
   );
 }
