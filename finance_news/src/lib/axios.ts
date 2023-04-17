7816041202b50236dfbebe3f7716cf7fcabfaf9c
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-16 23:59:33 -0300

    Corrige axios e scripts

--- finance_news/src/lib/axios.ts
+++ finance_news/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+// Troque a BASEURL pelo endereco gerado pelo LocalTunnel
+// Verifique package.json -> scripts.tunnel para mais detalhes
+
 export const api = axios.create({
-  baseURL: 'http://192.168.50.205:3000'
+  baseURL: 'https://fine-seals-kneel-190-115-199-98.loca.lt'
 })
