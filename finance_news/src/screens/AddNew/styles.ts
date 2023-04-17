f2281119fefadaad41b2d1ce329c03c36cfc1d41
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-04-16 23:01:24 -0300

    Adiciona Safe Area View nos componentes

--- finance_news/src/screens/AddNew/styles.ts
+++ finance_news/src/screens/AddNew/styles.ts
@@ -1,7 +1,8 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from "styled-components/native";
 import { CurrencyDollar } from 'phosphor-react-native'
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_500};
   padding: 24px;
